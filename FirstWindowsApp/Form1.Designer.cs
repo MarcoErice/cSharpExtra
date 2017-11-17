e58b11fbccfd2852be6c04cf1ac38d7f0a83adb8
MarcoErice <marco.erice@gmail.com>
2017-11-17 13:33:03 +0100

    added MouseClick event on text box.

---
+++ FirstWindowsApp/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace FirstWindowsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(132, 62);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(454, 31);
+            this.txtName.TabIndex = 0;
+            this.txtName.MouseClick += new System.Windows.Forms.MouseEventHandler(this.txtName_MouseClick);
+            this.txtName.TextChanged += new System.EventHandler(this.txtName_TextChanged);
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(132, 120);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(454, 31);
+            this.txtAge.TabIndex = 1;
+            this.txtAge.MouseClick += new System.Windows.Forms.MouseEventHandler(this.txtAge_MouseClick);
+            this.txtAge.TextChanged += new System.EventHandler(this.txtAge_TextChanged);
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(448, 199);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(138, 67);
+            this.btnSubmit.TabIndex = 2;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1124, 615);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.Button btnSubmit;
+    }
+}
+
---
+++ FirstWindowsApp/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FirstWindowsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            if (txtName.Text == string.Empty)
+            {
+                txtName.Text = "Name";
+            }
+            if (txtAge.Text == string.Empty)
+            {
+                txtAge.Text = "Age";
+            }
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            var name = txtName.Text;
+            //var age = txtAge.Text;
+            int age;
+            int.TryParse(txtAge.Text, out age);
+            var msg = string.Empty;
+            if (age > 0 && age < 13)
+            {
+                msg = $"{name} is a child.";
+            }
+            else if (age >= 13 && age <= 18)
+            {
+                msg = $"{name} is a teenager.";
+            }
+            else
+            {
+                msg = $"{name} is an adult.";
+            }
+            // var message = $"{name} is {age} years old.";
+            MessageBox.Show(msg);
+        }
+
+        private void txtName_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void txtAge_TextChanged(object sender, EventArgs e)
+        {
+                   
+        }
+
+        private void txtName_MouseClick(object sender, MouseEventArgs e)
+        {
+            txtName.Text = "";
+        }
+
+        private void txtAge_MouseClick(object sender, MouseEventArgs e)
+        {
+            txtAge.Text = "";
+        }
+    }
+}
