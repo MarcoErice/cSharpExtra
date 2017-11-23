30dfb79300374f6c7ba160f1c1ac14a3fc2e4105
MarcoErice <marco.erice@gmail.com>
2017-11-23 01:42:17 +0100

    Completed Weekdays.

--- Weekdays/Form1.Designer.cs
+++ Weekdays/Form1.Designer.cs
@@ -28,20 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.cboWeekdays = new System.Windows.Forms.ComboBox();
+            this.lstWeekdays = new System.Windows.Forms.ListBox();
+            this.lblAreEqual = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // cboWeekdays
+            // 
+            this.cboWeekdays.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboWeekdays.FormattingEnabled = true;
+            this.cboWeekdays.Location = new System.Drawing.Point(119, 55);
+            this.cboWeekdays.Name = "cboWeekdays";
+            this.cboWeekdays.Size = new System.Drawing.Size(180, 33);
+            this.cboWeekdays.TabIndex = 0;
+            this.cboWeekdays.SelectedIndexChanged += new System.EventHandler(this.Weekdays_SelectedIndexChanged);
+            // 
+            // lstWeekdays
+            // 
+            this.lstWeekdays.FormattingEnabled = true;
+            this.lstWeekdays.ItemHeight = 25;
+            this.lstWeekdays.Location = new System.Drawing.Point(119, 122);
+            this.lstWeekdays.Name = "lstWeekdays";
+            this.lstWeekdays.Size = new System.Drawing.Size(180, 229);
+            this.lstWeekdays.TabIndex = 1;
+            this.lstWeekdays.SelectedIndexChanged += new System.EventHandler(this.Weekdays_SelectedIndexChanged);
+            // 
+            // lblAreEqual
+            // 
+            this.lblAreEqual.AutoSize = true;
+            this.lblAreEqual.Location = new System.Drawing.Point(119, 373);
+            this.lblAreEqual.Name = "lblAreEqual";
+            this.lblAreEqual.Size = new System.Drawing.Size(70, 25);
+            this.lblAreEqual.TabIndex = 2;
+            this.lblAreEqual.Text = "label1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(914, 648);
+            this.Controls.Add(this.lblAreEqual);
+            this.Controls.Add(this.lstWeekdays);
+            this.Controls.Add(this.cboWeekdays);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox cboWeekdays;
+        private System.Windows.Forms.ListBox lstWeekdays;
+        private System.Windows.Forms.Label lblAreEqual;
     }
 }
 
--- Weekdays/Form1.cs
+++ Weekdays/Form1.cs
@@ -12,9 +12,36 @@ namespace Weekdays
 {
     public partial class Form1 : Form
     {
+        enum Weekday { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday }
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cboWeekdays.Items.AddRange(typeof(Weekday).GetEnumNames());
+            lstWeekdays.Items.AddRange(typeof(Weekday).GetEnumNames());
+
+            cboWeekdays.SelectedIndex = (int)DateTime.Today.DayOfWeek;
+            lstWeekdays.SelectedIndex = (int)DateTime.Today.DayOfWeek;
+        }
+
+        private void lstWeekdays_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Weekdays_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var cboDay = cboWeekdays.SelectedItem;
+            var lstDay = lstWeekdays.SelectedItem;
+
+            lblAreEqual.Text = String.Format("The selected days are equal: {0}", 
+            cboDay.Equals(lstDay));
+
+            lblAreEqual.BackColor = cboDay.Equals(lstDay) ? Color.LightGreen :
+            Color.LightPink;
         }
     }
 }
